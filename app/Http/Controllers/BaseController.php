92d33f24fd3f315a9c0f20305f6302c3ebfc2663
Norayr <noront90@gmail.com>
2019-07-18 16:51:21 +0400

    changes

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -9,6 +9,7 @@ class BaseController extends Controller
     {
         if(!is_null($files)) {
             $fileNames = [];
+            dd($files);
             foreach ($files as $file) {
                 $fileName = rand(0, 10000) . '_' . time() . '.' . $file->getClientOriginalExtension();
                 array_push($fileNames, $fileName);
