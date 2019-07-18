c36a30b943d053b108aeff63b175cb8112e5d981
Norayr <noront90@gmail.com>
2019-07-18 16:47:39 +0400

    changes

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -7,11 +7,11 @@ class BaseController extends Controller
 {
     public function fileUpload($files, $path)
     {
-        if(count($files) == 1){
+        if(count($files) == 1 && !is_null($files)){
             $fileName = rand(0, 10000) . '_' . time() . '.' . $files->getClientOriginalExtension();
             $files->move($path, $fileName);
             return $fileName;
-        }if(!is_null($files)) {
+        }else if(!is_null($files)) {
             $fileNames = [];
             foreach ($files as $file) {
                 $fileName = rand(0, 10000) . '_' . time() . '.' . $file->getClientOriginalExtension();
