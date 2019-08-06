96efabf850a56361840d51820f4b8348d3a3ec13
Norayr <noront90@gmail.com>
2019-08-06 15:32:48 +0400

    commented seeder tour types

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,16 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(TruncateTablesSeeder::class);
-//        $this->call(UserTableSeeder::class);
-//        $this->call(SettingTableSeed::class);
-//        $this->call(RegionTableSeeder::class);
-//        $this->call(BedTypeSeeder::class);
-//        $this->call(RegionImagesTableSeeder::class);
-//        $this->call(PageTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(SettingTableSeed::class);
+        $this->call(RegionTableSeeder::class);
+        $this->call(BedTypeSeeder::class);
+        $this->call(RegionImagesTableSeeder::class);
+        $this->call(PageTableSeeder::class);
 //        $this->call(TourTypesTableSeeder::class);
 //        $this->call(ToursTableSeeder::class);
 //        $this->call(TourImagesTableSeeder::class);
-//        $this->call(VehicleTypeTableSeeder::class);
+        $this->call(VehicleTypeTableSeeder::class);
 //        $this->call(CarDriverTableSeeder::class);
 //        $this->call(CarDriverSliderImagesTableSeeder::class);
 //        $this->call(NewsTableSeeder::class);
@@ -30,12 +30,12 @@ class DatabaseSeeder extends Seeder
 //        $this->call(ResidenceImagesTableSeeder::class);
 //        $this->call(RestaurantsTableSeeder::class);
 //        $this->call(RestaurantImagesTableSeeder::class);
-//        $this->call(RoomTypesTableSeeder::class);
+        $this->call(RoomTypesTableSeeder::class);
 //        $this->call(ServicesTableSeeder::class);
 //        $this->call(TeamsTableSeeder::class);
-//        $this->call(SettingTableSeed::class);
+        $this->call(SettingTableSeed::class);
 //        $this->call(VacancyTableSeeder::class);
-//        $this->call(GalleryTableSeeder::class);
-//        $this->call(CurrenciesTableSeeder::class);
+        $this->call(GalleryTableSeeder::class);
+        $this->call(CurrenciesTableSeeder::class);
     }
 }
