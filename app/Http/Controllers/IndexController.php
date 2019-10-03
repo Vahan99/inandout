de1c210067e0b695c41917c49c50bd292f612a60
Norayr <noront90@gmail.com>
2019-10-03 17:06:16 +0400

    change

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -430,13 +430,13 @@ class IndexController extends Controller
         }
 
         if(isset($request->room)) {
-            $residence_ids = \App\ResidenceRoomType::whereRoomTypeId($request->room)->get()->pluck('residence_id')->toArray();
-            $hotels = $hotels->whereIn('id', $residence_ids);
+            $r_residence_ids = \App\ResidenceRoomType::whereRoomTypeId($request->room)->get()->pluck('residence_id')->toArray();
+            $hotels = $hotels->whereIn('id', $r_residence_ids);
         }
 
         if(isset($request->bed)) {
-            $residence_ids = \App\ResidenceBedType::whereBedTypeId($request->bed)->get()->pluck('residence_id')->toArray();
-            $hotels = $hotels->whereIn('id', $residence_ids);
+            $b_residence_ids = \App\ResidenceBedType::whereBedTypeId($request->bed)->get()->pluck('residence_id')->toArray();
+            $hotels = $hotels->whereIn('id', $b_residence_ids);
         }
 
         $slug = $request->slug;
