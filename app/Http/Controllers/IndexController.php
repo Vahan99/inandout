c0ffff590f6ce6b16aefd7a2f9f1e4985728e77e
Vahan <vahan99mk@gmail.com>
2020-02-19 13:09:38 +0400

    bug fix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -156,7 +156,7 @@ class IndexController extends Controller
                 $data = json_decode($tour->data, true)['data'];
                 if(isset($data)){
                     foreach ($data as $d){
-                        dump(currency($d['price'], $request->currency, currency()->getUserCurrency()), "------", $d['price']);
+                        dump(currency($d['price'], $request->currency, currency()->getUserCurrency()), "------", $request->range_val);
                         currency($d['price'], $request->currency, currency()->getUserCurrency()) <= $request->range_val ? array_push($tour_ids, $tour->id) : false;
                     }
                 }
